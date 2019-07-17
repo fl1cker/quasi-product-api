83811083c8c10e242677a7d9e255694d5f991728
Aaron <the_flicker7@hotmail.com>
2019-07-17 13:03:16 -0400

    saving file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const debug = require('debug')('app');
 const app = express();
 const port = 3000;
 const productRouter = require('./src/routes/productRoutes.js');
+
 app.use(cors());
 app.use(morgan('tiny'));
 app.use('/products', productRouter);
